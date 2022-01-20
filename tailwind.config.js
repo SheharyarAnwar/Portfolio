da9c11bd41c527dd6a9148884fc6b460a8c965a0
Sheharyar Anwar <sheharyar28@hotmail.com>
2022-01-20 20:18:22 +0500

    refactor:removed redundant code

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.tsx", "./components/**/*.tsx", "./layouts/**/*.tsx"],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
