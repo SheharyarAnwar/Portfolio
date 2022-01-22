da0a44e91db7431db79e58069600d3e4fafb4e2a
Sheharyar Anwar <sheharyar28@hotmail.com>
2022-01-22 14:49:03 +0500

    feat:hero section done- unresponsive

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,12 +2,22 @@ import React from "react";
 import { Logo } from "../../public/assets/icons";
 export interface Props {}
 const Index: React.FC<Props> = () => {
-  return (
-    <div className="h-[100vh] bg-navy w-48">
-      <div className="flex flex-col">
-        <div className="mt-8 flex-center ">
-          <Logo width={60} />
+  const navigationItems = ["About", "Skills", "Work", "Blog", "Contact"].map(
+    (val) => {
+      return (
+        <div className="py-3 w-full hover:text-green cursor-pointer ">
+          <p className="text-center font-semibold">{val}</p>
         </div>
+      );
+    }
+  );
+  return (
+    <div className="h-[100vh] bg-navy w-48 fixed">
+      <div className="flex flex-col">
+        <div className="mt-10 flex-center ">
+          <Logo width={90} />
+        </div>
+        <nav className="flex-center flex-col mt-10">{navigationItems}</nav>
       </div>
     </div>
   );
