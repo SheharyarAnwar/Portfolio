419b341733612162e6570c212dd9a68dcc59e349
Sheharyar Anwar <sheharyar28@hotmail.com>
2022-01-26 21:59:38 +0500

    feat: more responsivness for project card

--- components/ProjectCard/index.tsx
+++ components/ProjectCard/index.tsx
@@ -4,41 +4,55 @@ interface Props {
   reversed?: Boolean;
 }
 const Index: React.FC<Props> = ({ reversed = false }) => {
-  let rightClass = "col-start-1 col-span-7 row-start-1";
-  let leftClass = "col-start-7 col-span-6 row-start-1 z-10";
+  let leftClass =
+    "col-start-2 row-start-2 col-span-10 lg:col-start-1 lg:col-span-7 lg:row-start-1";
+  let rightClass =
+    "col-start-1 row-start-1 col-span-12 lg:col-start-7 lg:col-span-6 lg:row-start-1 z-10";
   if (reversed) {
-    leftClass = "col-start-1 col-span-6 row-start-1 z-10";
-    rightClass = "col-start-6 col-span-7 row-start-1";
+    rightClass =
+      "col-start-1 row-start-1 col-span-12 lg:col-start-1 lg:col-span-6 lg:row-start-1 z-10";
+    leftClass =
+      "col-start-2 row-start-2 col-span-10 lg:col-start-6 lg:col-span-7 lg:row-start-1";
   }
   return (
     <div className="grid grid-cols-12 py-16 items-center">
-      <div className={rightClass}>
+      <div className={leftClass + " relative"}>
+        {/* {TODO: Carousel} */}
         <Image
           src="/assets/images/Templific/background.png"
           height={768}
           width={1366}
-          objectFit="fill"
+          layout="responsive"
           alt="bg"
           priority
         />
+        {/* <div
+          className={`absolute w-full h-full top-0 left-0 bg-navy opacity-80 hover:opacity-0`}
+        ></div> */}
       </div>
-      <div className={leftClass + " h-full grid grid-cols-12 grid-rows-3"}>
+      <div
+        className={
+          rightClass + " h-full grid grid-cols-12 grid-rows-3 relative"
+        }
+      >
         <div
-          className={`col-span-full self-center justify-self-${
+          className={`col-span-full self-center justify-self-center lg:justify-self-${
             reversed ? "start" : "end"
           }`}
         >
-          <p className="text-green text-base mb-2">Web application</p>
-          <h3 className="">Templific</h3>
+          <p className="text-center lg:text-left text-green text-base mb-2">
+            Web application
+          </p>
+          <h3 className="text-center lg:text-left">Templific</h3>
         </div>
-        <p className="p-8 bg-navy-accent col-span-12 self-center">
+        <p className="p-8 lg:bg-navy-accent col-span-12 self-center">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae
           illo magni reiciendis dicta dolore at officia aliquam blanditiis
           molestias,
         </p>
 
         <div
-          className={`col-span-full justify-self-${
+          className={`col-span-full justify-self-center lg:justify-self-${
             reversed ? "start" : "end"
           } self-center flex mb-2 gap-8 font-mono text-green`}
         >
