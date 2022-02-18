04981dc0040f800b0e6d3d9f273c0a685769955e
Sheharyar Anwar <sheharyar28@hotmail.com>
2022-02-18 19:22:47 +0500

    added links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { IconContainer } from "..";
 import {
@@ -6,9 +7,9 @@ import {
   GithubIcon,
   Logo,
   MailIcon,
-  // TwitterIcon,
   WhatsappIcon,
 } from "../../../public/assets/icons";
+import { socialLinks } from "../../constants";
 interface Props {}
 const Index: React.FC<Props> = () => {
   return (
@@ -22,21 +23,51 @@ const Index: React.FC<Props> = () => {
         </h4>
         <h4 className="text-center mb-10 text-gray-300"> one day at a time.</h4>
         <div className="flex flex-center gap-4 flex-wrap">
-          <IconContainer>
-            <GithubIcon />
-          </IconContainer>
-          <IconContainer>
-            <FacebookIcon />
-          </IconContainer>
-          <IconContainer>
-            <WhatsappIcon />
-          </IconContainer>{" "}
-          <IconContainer>
-            <MailIcon />
-          </IconContainer>{" "}
-          <IconContainer>
-            <CodepenIcon />
-          </IconContainer>
+          <a
+            href={socialLinks.github}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IconContainer>
+              <GithubIcon />
+            </IconContainer>
+          </a>
+          <a
+            href={socialLinks.linkedin}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IconContainer>
+              <FacebookIcon />
+            </IconContainer>
+          </a>
+          <a
+            href={socialLinks.whatsapp}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IconContainer>
+              <WhatsappIcon />
+            </IconContainer>
+          </a>
+          <a
+            href={"mailto:" + socialLinks.email}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IconContainer>
+              <MailIcon />
+            </IconContainer>
+          </a>
+          <a
+            href={socialLinks.codepen}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IconContainer>
+              <CodepenIcon />
+            </IconContainer>
+          </a>
         </div>
       </div>
     </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -6,3 +6,12 @@ exports.breakpoints = {
   xl: 1280,
   "2xl": 1536,
 };
+
+exports.socialLinks = {
+  email: "sheharyar28@hotmail.com",
+  github: "https://github.com/SheharyarAnwar",
+  facebook: "https://facebook.com/sheharyar.anwar",
+  codepen: "https://codepen.io/sherryLooper",
+  whatsapp: "https://wa.me/923074586346",
+  linkedin: "https://www.linkedin.com/in/sheharyar-anwar",
+};
